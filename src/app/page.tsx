af99fd522efc55494ce2d9581d0d99971d93a9b3
Himanshi Sharma <himanshish456@gmail.com>
2024-04-20 12:02:40 +0530

    navbar

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.0]">
+      <h1 className="text-2xl text-center">hello</h1>
+    </main>
+  );
+}
